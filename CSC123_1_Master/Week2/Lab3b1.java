99476797692fc7d66808e964bc8e1f9ec4a56ee8
headhoncho <headhoncho@localhost>
2023-02-03 00:40:59 -0800

    lab 3 number 1 and 2

---
+++ CSC123_1_Master/Week2/Lab3b1.java
@@ -0,0 +1,13 @@
+
+public class Lab3b1 {
+
+	public static void main(String[] args) {
+		
+		//int x = 130;
+		//byte y = (byte) x;
+		//System.out.println(x);
+		
+	
+	}
+
+}
---
+++ CSC123_1_Master/Week2/Lab3b2.java
@@ -0,0 +1,13 @@
+
+public class Lab3b2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 99; i++)
+		{
+			if(i % 2 == 0)
+			continue;
+			System.out.print(i+" ");
+		}
+	}
+}
