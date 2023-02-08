6d58b4500c02f9aa8b66404166ebee7faa99c58b
headhoncho <headhoncho@heads-mbp.attlocal.net>
2023-02-07 23:40:21 -0800

    finished copy submitting again

--- CSC123_1_Master/Week3/assignment2/TipCalculator.java
+++ CSC123_1_Master/Week3/assignment2/TipCalculator.java
@@ -13,30 +13,30 @@ public class TipCalculator {
 			DecimalFormat df = new DecimalFormat(".00");
 			
 			int happiness;
-			double billAmount;
+			double bill;
 			double tips = 0;
 			double total;
 			double tax = 0.0713;
 			
-			double[] tipsPercentages = {5,0.075,10,15,20};
+			double[] tips_percentages = {5,0.075,10,15,20};
 			
 			System.out.print("Enter bill amount: ");
-			billAmount = input.nextDouble();
+			bill = input.nextDouble();
 			
-			total = billAmount * tax + billAmount;
+			total = bill * tax + bill;
 			
 			System.out.print("How happy are you with the service [1-5] : ");
 			happiness = input.nextInt();
 			
 			if(happiness >= 1 && happiness <= 5)
 			{
-				tips = total * tipsPercentages[happiness - 1]/100;
+				tips = total * tips_percentages[happiness - 1]/100;
 			}
 			
 			df.setRoundingMode(RoundingMode.DOWN);
-			System.out.printf("%-40s %1s \n","Your bill total",": " + billAmount);
-			System.out.printf("%-40s %1s \n","Tax (@7.13%)",": " + df.format(billAmount * tax));
-			System.out.printf("%-40s %1s %.2f \n" , "Tip (@" + tipsPercentages[happiness - 1] + "%) based on happiness value " + happiness, ":", tips);
+			System.out.printf("%-40s %1s \n","Your bill total",": " + bill);
+			System.out.printf("%-40s %1s \n","Tax (@7.13%)",": " + df.format(bill * tax));
+			System.out.printf("%-40s %1s %.2f \n" , "Tip (@" + tips_percentages[happiness - 1] + "%) based on happiness value " + happiness, ":", tips);
 			System.out.printf("%-40s %1s ","Total Payable",": " + df.format(total + tips));
 			
 			input.close();
