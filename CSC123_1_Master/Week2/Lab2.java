bf81dc26c2a965f246d9c858e86d7a166f3e1cd1
headhoncho <headhoncho@localhost>
2023-02-08 11:14:26 -0800

    fixed lab2

--- CSC123_1_Master/Week2/Lab2.java
+++ CSC123_1_Master/Week2/Lab2.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Lab2 {
 
 	public static void main(String[] args) {
+				
+				Scanner input = new Scanner(System.in);
+				
+				System.out.println("Please enter an integer: ");
+				int num = input.nextInt();
+				System.out.println("Here is a " + num + " x " + num + " multiplication table:");
+				
+				int i=1;
+				while(i<=num){ 
+				int j=1; 
+				    while(j<=num)
+				    {
+				     System.out.print(i*j+"\t");
+				     j++;
+				    }
+				            i++;
+				            System.out.println();
+				}
+							input.close();
 	}
 }
