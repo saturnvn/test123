e1e1ac2601be94e181d0cc5ab55c285f6f93c1da
headhoncho <headhoncho@heads-mbp.attlocal.net>
2023-02-16 17:09:44 -0800

    q1 done

--- CSC123_1_Master/Week4/InvString.java
+++ CSC123_1_Master/Week4/InvString.java
@@ -3,8 +3,15 @@ public class InvString {
 
 	public static void main(String[] args) {
 		
+		String string = "This is a test";
 		
+		char[] array = string.toCharArray();
+		
+		for(int i = array.length - 1; i >= 0; i--)
+		{
+			System.out.print(array[i]);
+		}	
 
 	}
-
+	
 }
